8754397f5b5fc8fd68c1758a863a888553239b76
Boyko Kazakov <bkazakov@luxoft.com>
2017-02-03 14:14:45 +0200

    bmp  adafruit crap

--- luxTemperature/luxTemperature.ino
+++ luxTemperature/luxTemperature.ino
@@ -1,18 +1,33 @@
 /*
- * Required libraries
- * Adafruit_BMP280 
- * FastLed
- * 
- */
+   Required libraries
+   Adafruit_BMP280
+   FastLed
+
+*/
+
+#include <Wire.h>
+#include <SPI.h>
+//#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
 #include "FastLED.h"
+
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+Adafruit_BMP280 bme; // I2C
+//Adafruit_BMP280 bme(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 FASTLED_USING_NAMESPACE
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
-#define DATA_PIN    12
+#define DATA_PIN    12 //TODO change
 
 #define LED_TYPE    WS2812
 #define COLOR_ORDER RGB
@@ -22,36 +37,61 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
 
-uint8_t gHue = 0; 
+uint8_t gHue = 0;
 
 void setup() {
- delay(2000); 
-  
-  
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  delay(2000);
+
+
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
 
 }
 
 void loop() {
+  bmpTest();
 
-  FastLED.show();  
+  FastLED.show();
   // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { sinelon(); } // change patterns periodically
+  EVERY_N_MILLISECONDS( 20 ) {
+    gHue++;  // slowly cycle the "base color" through the rainbow
+  }
+  EVERY_N_SECONDS( 10 ) {
+    sinelon();  // change patterns periodically
+  }
 
 }
 
+void bmpTest() {
 
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Approx altitude = ");
+  Serial.print(bme.readAltitude(1013.25)); // this should be adjusted to your local forcase
+  Serial.println(" m");
+
+  Serial.println();
+}
 
 void sinelon()
 {
   // a colored dot sweeping back and forth, with fading trails
   fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16(13,0,NUM_LEDS);
+  int pos = beatsin16(13, 0, NUM_LEDS);
   leds[pos] += CHSV( gHue, 255, 192);
 }
 
